c9fed3d2e0ee8c946165d185568f77d7e55d2048
Artur Nikitin <branewor@gmail.com>
2020-09-16 16:21:16 +0400

    первый экран + слайдер

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.prof__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+      });
+          
+  });
